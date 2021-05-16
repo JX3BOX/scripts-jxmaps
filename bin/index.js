03fb1e5940609e24a62509764b585b40ee0520e7
Kaviilee <jiaweilee@126.com>
2021-05-16 18:26:51 +0800

    chore: remove unused package

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,6 @@
 const path = require('path')
 const fs = require('fs')
 const { program } = require('commander')
-// const initial = require('../packages/commands/initial')
 const actions = require('../packages/commands/actions')
 
 let config = {}
